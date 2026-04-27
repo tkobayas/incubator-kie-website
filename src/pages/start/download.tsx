ae95fbc58257d75c3865a0a35c22aa5446c22728
Tiago Bento <tiagobentofernandes@gmail.com>
2026-04-27 15:44:28 -0400

    Add /start/download redirect

---
+++ src/pages/start/download.tsx
@@ -0,0 +1,5 @@
+import {Redirect} from '@docusaurus/router';
+
+export default function StartDownloadRedirect(): JSX.Element {
+  return <Redirect to="/downloads/download_10_1_0" />;
+}
---
+++ src/pages/start/download_10_0_0.tsx
@@ -0,0 +1,5 @@
+import {Redirect} from '@docusaurus/router';
+
+export default function StartDownload1000Redirect(): JSX.Element {
+  return <Redirect to="/downloads/download_10_0_0" />;
+}
